29d68239326acec3c1ca9196e0e0df85b8bafaa1
GrzesiuS <grzesius2@gmail.com>
2012-03-26 09:05:46 +0200

    nowe pliki

---
+++ Desktop/mainWindow.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace DziennikLekcyjny
+{
+    partial class mainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.mainMenu = new System.Windows.Forms.MenuStrip();
+            this.plikToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.wylogujToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.zakończToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.edycjaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.preferencjeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pomocToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.oProgramieToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusBar = new System.Windows.Forms.StatusStrip();
+            this.dateTime = new System.Windows.Forms.Timer(this.components);
+            this.tabs = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.dataGrid1 = new System.Windows.Forms.DataGridView();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.tabPage6 = new System.Windows.Forms.TabPage();
+            this.calendar = new System.Windows.Forms.MonthCalendar();
+            this.dataGrid2 = new System.Windows.Forms.DataGridView();
+            this.mainMenu.SuspendLayout();
+            this.tabs.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mainMenu
+            // 
+            this.mainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.plikToolStripMenuItem,
+            this.edycjaToolStripMenuItem,
+            this.pomocToolStripMenuItem});
+            this.mainMenu.Location = new System.Drawing.Point(0, 0);
+            this.mainMenu.Name = "mainMenu";
+            this.mainMenu.Size = new System.Drawing.Size(947, 24);
+            this.mainMenu.TabIndex = 0;
+            this.mainMenu.Text = "menuStrip1";
+            // 
+            // plikToolStripMenuItem
+            // 
+            this.plikToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.wylogujToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.zakończToolStripMenuItem});
+            this.plikToolStripMenuItem.Name = "plikToolStripMenuItem";
+            this.plikToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.plikToolStripMenuItem.Text = "Plik";
+            // 
+            // wylogujToolStripMenuItem
+            // 
+            this.wylogujToolStripMenuItem.Name = "wylogujToolStripMenuItem";
+            this.wylogujToolStripMenuItem.Size = new System.Drawing.Size(118, 22);
+            this.wylogujToolStripMenuItem.Text = "Wyloguj";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(115, 6);
+            // 
+            // zakończToolStripMenuItem
+            // 
+            this.zakończToolStripMenuItem.Name = "zakończToolStripMenuItem";
+            this.zakończToolStripMenuItem.Size = new System.Drawing.Size(118, 22);
+            this.zakończToolStripMenuItem.Text = "Zakończ";
+            this.zakończToolStripMenuItem.Click += new System.EventHandler(this.zakończToolStripMenuItem_Click);
+            // 
+            // edycjaToolStripMenuItem
+            // 
+            this.edycjaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.preferencjeToolStripMenuItem});
+            this.edycjaToolStripMenuItem.Name = "edycjaToolStripMenuItem";
+            this.edycjaToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.edycjaToolStripMenuItem.Text = "Edycja";
+            // 
+            // preferencjeToolStripMenuItem
+            // 
+            this.preferencjeToolStripMenuItem.Name = "preferencjeToolStripMenuItem";
+            this.preferencjeToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.preferencjeToolStripMenuItem.Text = "Preferencje";
+            // 
+            // pomocToolStripMenuItem
+            // 
+            this.pomocToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.oProgramieToolStripMenuItem});
+            this.pomocToolStripMenuItem.Name = "pomocToolStripMenuItem";
+            this.pomocToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.pomocToolStripMenuItem.Text = "Pomoc";
+            // 
+            // oProgramieToolStripMenuItem
+            // 
+            this.oProgramieToolStripMenuItem.Name = "oProgramieToolStripMenuItem";
+            this.oProgramieToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
+            this.oProgramieToolStripMenuItem.Text = "O programie";
+            // 
+            // statusBar
+            // 
+            this.statusBar.Location = new System.Drawing.Point(0, 511);
+            this.statusBar.Name = "statusBar";
+            this.statusBar.Size = new System.Drawing.Size(947, 22);
+            this.statusBar.TabIndex = 1;
+            // 
+            // dateTime
+            // 
+            this.dateTime.Enabled = true;
+            this.dateTime.Interval = 1000;
+            this.dateTime.Tick += new System.EventHandler(this.dateTime_Tick);
+            // 
+            // tabs
+            // 
+            this.tabs.Controls.Add(this.tabPage1);
+            this.tabs.Controls.Add(this.tabPage2);
+            this.tabs.Controls.Add(this.tabPage3);
+            this.tabs.Controls.Add(this.tabPage4);
+            this.tabs.Controls.Add(this.tabPage5);
+            this.tabs.Controls.Add(this.tabPage6);
+            this.tabs.Location = new System.Drawing.Point(0, 24);
+            this.tabs.Name = "tabs";
+            this.tabs.SelectedIndex = 0;
+            this.tabs.Size = new System.Drawing.Size(747, 487);
+            this.tabs.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGrid1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Size = new System.Drawing.Size(739, 461);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // dataGrid1
+            // 
+            this.dataGrid1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGrid1.Location = new System.Drawing.Point(0, 0);
+            this.dataGrid1.Name = "dataGrid1";
+            this.dataGrid1.Size = new System.Drawing.Size(739, 461);
+            this.dataGrid1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGrid2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Size = new System.Drawing.Size(739, 461);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(739, 461);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(739, 461);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "tabPage4";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Location = new System.Drawing.Point(4, 22);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Size = new System.Drawing.Size(739, 461);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "tabPage5";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // tabPage6
+            // 
+            this.tabPage6.Location = new System.Drawing.Point(4, 22);
+            this.tabPage6.Name = "tabPage6";
+            this.tabPage6.Size = new System.Drawing.Size(739, 461);
+            this.tabPage6.TabIndex = 5;
+            this.tabPage6.Text = "tabPage6";
+            this.tabPage6.UseVisualStyleBackColor = true;
+            // 
+            // calendar
+            // 
+            this.calendar.CalendarDimensions = new System.Drawing.Size(1, 2);
+            this.calendar.Location = new System.Drawing.Point(755, 46);
+            this.calendar.MaxDate = new System.DateTime(2100, 12, 31, 0, 0, 0, 0);
+            this.calendar.MinDate = new System.DateTime(2010, 1, 1, 0, 0, 0, 0);
+            this.calendar.Name = "calendar";
+            this.calendar.ShowWeekNumbers = true;
+            this.calendar.TabIndex = 3;
+            // 
+            // dataGrid2
+            // 
+            this.dataGrid2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGrid2.Location = new System.Drawing.Point(0, 0);
+            this.dataGrid2.Name = "dataGrid2";
+            this.dataGrid2.Size = new System.Drawing.Size(739, 461);
+            this.dataGrid2.TabIndex = 0;
+            // 
+            // mainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(947, 533);
+            this.Controls.Add(this.calendar);
+            this.Controls.Add(this.tabs);
+            this.Controls.Add(this.statusBar);
+            this.Controls.Add(this.mainMenu);
+            this.DoubleBuffered = true;
+            this.MainMenuStrip = this.mainMenu;
+            this.Name = "mainWindow";
+            this.Text = "e-dziennik";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.mainWindow_FormClosing);
+            this.mainMenu.ResumeLayout(false);
+            this.mainMenu.PerformLayout();
+            this.tabs.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mainMenu;
+        private System.Windows.Forms.ToolStripMenuItem plikToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zakończToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem edycjaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pomocToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem oProgramieToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusBar;
+        private System.Windows.Forms.Timer dateTime;
+        private System.Windows.Forms.TabControl tabs;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.TabPage tabPage6;
+        private System.Windows.Forms.ToolStripMenuItem wylogujToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem preferencjeToolStripMenuItem;
+        private System.Windows.Forms.MonthCalendar calendar;
+        private System.Windows.Forms.DataGridView dataGrid1;
+        private System.Windows.Forms.DataGridView dataGrid2;
+    }
+}
---
+++ Desktop/mainWindow.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DziennikLekcyjny
+{
+    public partial class mainWindow : Form
+    {
+        private byte tabsCount = 6;
+        private string[] tabsNames = { "Frekwencja", "Oceny", "Tematy zajęć", "Uwagi", "Ogłoszenia", "Plan zajęć" };
+        private int rowCount = 10, columnCount = 20;
+        private List<dataGridSettings> settings = new List<dataGridSettings>(); //lista ustawien wyswietlania dataGridView
+        /*ZAKŁADKI :
+         * frekwencja
+         * oceny
+         * tematy zajęć
+         * uwagi
+         * ogłoszenia
+         * plan zajęć
+         * kalendarz
+         */
+
+        public mainWindow()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < tabsCount; ++i)
+            {
+                tabs.TabPages[i].Text = tabsNames[i];
+            }
+            statusBar.Items.Add("");                //data i czas
+
+            //lista ustawien;
+
+            settings.Add(new dataGridSettings());   //frekwencja            
+            settings[0].columnCount = 50;           // 5 dni po max 10 lekcji
+            settings[0].rowCount = 15;              // narazie nie wiadomo ilu bedzie uczniow
+            settings[0].rowHeaderSize = 150;
+            settings[0].columnWidth = 15;
+            settings[0].offset = new Point(0, 0);
+
+            settings.Add(new dataGridSettings());   //oceny
+            settings[1].columnCount = 30;           
+            settings[1].rowCount = 15;              // narazie nie wiadomo ilu bedzie uczniow
+            settings[1].rowHeaderSize = 150;
+            settings[1].columnWidth = 20;
+            settings[1].offset = new Point(0, 0);
+
+            dataGrid1.Location = addPoints( dataGrid1.Location, settings[0].offset );
+            dataGrid1.ColumnCount = settings[0].columnCount;
+            dataGrid1.RowCount = settings[0].rowCount;
+            dataGrid1.RowHeadersWidth = settings[0].rowHeaderSize;
+            dataGrid1.ColumnHeadersVisible = false;
+
+            dataGrid2.Location = addPoints(dataGrid2.Location, settings[1].offset);
+            dataGrid2.ColumnCount = settings[1].columnCount;
+            dataGrid2.RowCount = settings[1].rowCount;
+            dataGrid2.RowHeadersWidth = settings[1].rowHeaderSize;
+            dataGrid2.ColumnHeadersVisible = true;
+
+            for (int i = 0; i < dataGrid1.ColumnCount; ++i)
+                dataGrid1.Columns[i].Width = settings[0].columnWidth;
+
+            for (int i = 0; i < dataGrid2.ColumnCount; ++i)
+                dataGrid2.Columns[i].Width = settings[1].columnWidth;
+
+            
+
+            for (int i = 0; i < dataGrid1.RowCount; ++i)
+                dataGrid1.Rows[i].HeaderCell.Value = "Imie i nazwisko";
+
+            for (int i = 0; i < dataGrid2.RowCount; ++i)
+                dataGrid2.Rows[i].HeaderCell.Value = "Imie i nazwisko";
+            
+
+            dataGrid1.AllowUserToResizeColumns = dataGrid2.AllowUserToResizeColumns = false;
+            dataGrid1.AllowUserToResizeRows = dataGrid2.AllowUserToResizeRows = false;
+        }
+
+        private void zakończToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void mainWindow_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void dateTime_Tick(object sender, EventArgs e)
+        {
+            statusBar.Items[0].Text = DateTime.Now.ToString();
+        }
+
+        private Point addPoints(Point a, Point b)
+        {
+            return new Point(a.X + b.X, a.Y + b.Y);
+        }
+
+    }
+}
