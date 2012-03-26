87fa697f1bb443f0c3e32c115693ef8a663f6d90
msatora <sebo_black666@o2.pl>
2012-03-26 09:54:46 +0200

    Oceny Maks

---
+++ Desktop/Oceny.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DziennikLekcyjny
+{
+    public class Oceny
+    {
+        public void PobierzOceny()
+        { 
+        
+        }
+    }
+}
