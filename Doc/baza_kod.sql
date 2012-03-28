a12b98ee0748ea57f39c860872fec14e4d0dc3dd
Kamil Pomykala <ja@kamilpomykala.com>
2012-03-28 11:38:04 +0200

    aktualizacja struktury bazy danych

--- Doc/baza_kod.sql
+++ Doc/baza_kod.sql
@@ -2,9 +2,6 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
-DROP SCHEMA IF EXISTS `kamilpomyk_baza` ;
-CREATE SCHEMA IF NOT EXISTS `kamilpomyk_baza` DEFAULT CHARACTER SET utf8 COLLATE utf8_polish_ci ;
-USE `kamilpomyk_baza` ;
 
 -- -----------------------------------------------------
 -- Table `kamilpomyk_baza`.`school`
@@ -35,11 +32,10 @@ CREATE  TABLE IF NOT EXISTS `kamilpomyk_baza`.`person` (
   `city` VARCHAR(20) NOT NULL ,
   `email` VARCHAR(30) NOT NULL ,
   `phone` INT NOT NULL ,
-  `type` ENUM('T', 'S', 'E') NOT NULL ,
   `school_id` INT NOT NULL ,
   PRIMARY KEY (`id`) ,
   INDEX `school_id` (`school_id` ASC) ,
-  CONSTRAINT 
+  CONSTRAINT
     FOREIGN KEY (`school_id` )
     REFERENCES `kamilpomyk_baza`.`school` (`id` )
     ON DELETE NO ACTION
@@ -58,7 +54,7 @@ CREATE  TABLE IF NOT EXISTS `kamilpomyk_baza`.`employee` (
   `person_id` INT NOT NULL ,
   PRIMARY KEY (`id`) ,
   INDEX `person_id` (`person_id` ASC) ,
-  CONSTRAINT 
+  CONSTRAINT
     FOREIGN KEY (`person_id` )
     REFERENCES `kamilpomyk_baza`.`person` (`id` )
     ON DELETE NO ACTION
@@ -76,7 +72,7 @@ CREATE  TABLE IF NOT EXISTS `kamilpomyk_baza`.`teacher` (
   `person_id` INT NOT NULL ,
   PRIMARY KEY (`id`) ,
   INDEX `person_id` (`person_id` ASC) ,
-  CONSTRAINT 
+  CONSTRAINT
     FOREIGN KEY (`person_id` )
     REFERENCES `kamilpomyk_baza`.`person` (`id` )
     ON DELETE NO ACTION
@@ -91,12 +87,20 @@ DROP TABLE IF EXISTS `kamilpomyk_baza`.`class` ;
 
 CREATE  TABLE IF NOT EXISTS `kamilpomyk_baza`.`class` (
   `id` INT NOT NULL ,
+  `name` VARCHAR(5) NOT NULL ,
   `class_teacher_id` INT NOT NULL ,
+  `school_id` INT NOT NULL ,
   PRIMARY KEY (`id`) ,
   INDEX `class_teacher_id` (`class_teacher_id` ASC) ,
-  CONSTRAINT 
+  INDEX `school_id` (`school_id` ASC) ,
+  CONSTRAINT
     FOREIGN KEY (`class_teacher_id` )
     REFERENCES `kamilpomyk_baza`.`teacher` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT
+    FOREIGN KEY (`school_id` )
+    REFERENCES `kamilpomyk_baza`.`school` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -114,7 +118,7 @@ CREATE  TABLE IF NOT EXISTS `kamilpomyk_baza`.`student` (
   PRIMARY KEY (`id`) ,
   INDEX `person_id` (`person_id` ASC) ,
   INDEX `class_id` (`class_id` ASC) ,
-  CONSTRAINT 
+  CONSTRAINT
     FOREIGN KEY (`person_id` )
     REFERENCES `kamilpomyk_baza`.`person` (`id` )
     ON DELETE NO ACTION
@@ -154,7 +158,7 @@ CREATE  TABLE IF NOT EXISTS `kamilpomyk_baza`.`grades` (
   PRIMARY KEY (`id`) ,
   INDEX `student_id` (`student_id` ASC) ,
   INDEX `subject_id` (`subject_id` ASC) ,
-  CONSTRAINT 
+  CONSTRAINT
     FOREIGN KEY (`student_id` )
     REFERENCES `kamilpomyk_baza`.`student` (`id` )
     ON DELETE NO ACTION
@@ -193,17 +197,18 @@ CREATE  TABLE IF NOT EXISTS `kamilpomyk_baza`.`access` (
   PRIMARY KEY (`id`) ,
   INDEX `person_id` (`person_id` ASC) ,
   INDEX `role_id` (`role_id` ASC) ,
-  CONSTRAINT 
+  CONSTRAINT
     FOREIGN KEY (`person_id` )
     REFERENCES `kamilpomyk_baza`.`person` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT 
+  CONSTRAINT
     FOREIGN KEY (`role_id` )
     REFERENCES `kamilpomyk_baza`.`role` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
+
 
 -- -----------------------------------------------------
 -- Table `kamilpomyk_baza`.`teacher_subject`
@@ -214,6 +219,7 @@ CREATE  TABLE IF NOT EXISTS `kamilpomyk_baza`.`teacher_subject` (
   `id` INT NOT NULL ,
   `teacher_id` INT NOT NULL ,
   `subject_id` INT NOT NULL ,
+  `password` VARCHAR(45) NOT NULL ,
   PRIMARY KEY (`id`) ,
   INDEX `teacher_id` (`teacher_id` ASC) ,
   INDEX `subject_id` (`subject_id` ASC) ,
@@ -238,7 +244,14 @@ DROP TABLE IF EXISTS `kamilpomyk_baza`.`classroom` ;
 CREATE  TABLE IF NOT EXISTS `kamilpomyk_baza`.`classroom` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `name` VARCHAR(5) NOT NULL ,
-  PRIMARY KEY (`id`) )
+  `school_id` INT NOT NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `school_id` (`school_id` ASC) ,
+  CONSTRAINT 
+    FOREIGN KEY (`school_id` )
+    REFERENCES `kamilpomyk_baza`.`school` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
@@ -268,7 +281,7 @@ CREATE  TABLE IF NOT EXISTS `kamilpomyk_baza`.`lesson` (
     REFERENCES `kamilpomyk_baza`.`class` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT 
+  CONSTRAINT
     FOREIGN KEY (`classroom_id` )
     REFERENCES `kamilpomyk_baza`.`classroom` (`id` )
     ON DELETE NO ACTION
@@ -328,7 +341,7 @@ CREATE  TABLE IF NOT EXISTS `kamilpomyk_baza`.`role_resource` (
     REFERENCES `kamilpomyk_baza`.`role` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT 
+  CONSTRAINT
     FOREIGN KEY (`resource_id` )
     REFERENCES `kamilpomyk_baza`.`resource` (`id` )
     ON DELETE NO ACTION
@@ -348,7 +361,7 @@ CREATE  TABLE IF NOT EXISTS `kamilpomyk_baza`.`comment` (
   `student_id` INT NOT NULL ,
   PRIMARY KEY (`id`) ,
   INDEX `student_id` (`student_id` ASC) ,
-  CONSTRAINT 
+  CONSTRAINT
     FOREIGN KEY (`student_id` )
     REFERENCES `kamilpomyk_baza`.`student` (`id` )
     ON DELETE NO ACTION
@@ -369,12 +382,12 @@ CREATE  TABLE IF NOT EXISTS `kamilpomyk_baza`.`group` (
   PRIMARY KEY (`id`) ,
   INDEX `teacher_subject_id` (`teacher_subject_id` ASC) ,
   INDEX `class_id` (`class_id` ASC) ,
-  CONSTRAINT 
+  CONSTRAINT
     FOREIGN KEY (`teacher_subject_id` )
     REFERENCES `kamilpomyk_baza`.`teacher_subject` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT 
+  CONSTRAINT
     FOREIGN KEY (`class_id` )
     REFERENCES `kamilpomyk_baza`.`class` (`id` )
     ON DELETE NO ACTION
@@ -392,12 +405,12 @@ CREATE  TABLE IF NOT EXISTS `kamilpomyk_baza`.`student_group` (
   `group_id` INT NOT NULL ,
   INDEX `student_id` (`student_id` ASC) ,
   INDEX `group_id` (`group_id` ASC) ,
-  CONSTRAINT 
+  CONSTRAINT
     FOREIGN KEY (`student_id` )
     REFERENCES `kamilpomyk_baza`.`student` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT 
+  CONSTRAINT
     FOREIGN KEY (`group_id` )
     REFERENCES `kamilpomyk_baza`.`group` (`id` )
     ON DELETE NO ACTION
@@ -420,6 +433,109 @@ CREATE  TABLE IF NOT EXISTS `kamilpomyk_baza`.`lesson_history` (
   CONSTRAINT 
     FOREIGN KEY (`lesson_id` )
     REFERENCES `kamilpomyk_baza`.`lesson` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `kamilpomyk_baza`.`message_text`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `kamilpomyk_baza`.`message_text` ;
+
+CREATE  TABLE IF NOT EXISTS `kamilpomyk_baza`.`message_text` (
+  `id` INT NOT NULL ,
+  `text` BLOB NOT NULL ,
+  PRIMARY KEY (`id`) )
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `kamilpomyk_baza`.`message`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `kamilpomyk_baza`.`message` ;
+
+CREATE  TABLE IF NOT EXISTS `kamilpomyk_baza`.`message` (
+  `id` INT NOT NULL ,
+  `date` INT NOT NULL ,
+  `sender_id` INT NOT NULL ,
+  `recipient_id` INT NOT NULL ,
+  `text_id` INT NOT NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `sender_id` (`sender_id` ASC) ,
+  INDEX `recipient_id` (`recipient_id` ASC) ,
+  INDEX `text_id` (`text_id` ASC) ,
+  CONSTRAINT 
+    FOREIGN KEY (`sender_id` )
+    REFERENCES `kamilpomyk_baza`.`person` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT
+    FOREIGN KEY (`recipient_id` )
+    REFERENCES `kamilpomyk_baza`.`person` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT 
+    FOREIGN KEY (`text_id` )
+    REFERENCES `kamilpomyk_baza`.`message_text` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `kamilpomyk_baza`.`person_type`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `kamilpomyk_baza`.`person_type` ;
+
+CREATE  TABLE IF NOT EXISTS `kamilpomyk_baza`.`person_type` (
+  `person_id` INT NOT NULL ,
+  `type` ENUM('T', 'S', 'E', 'G') NOT NULL ,
+  INDEX `person_id` (`person_id` ASC) ,
+  CONSTRAINT 
+    FOREIGN KEY (`person_id` )
+    REFERENCES `kamilpomyk_baza`.`person` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `kamilpomyk_baza`.`guardian`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `kamilpomyk_baza`.`guardian` ;
+
+CREATE  TABLE IF NOT EXISTS `kamilpomyk_baza`.`guardian` (
+  `id` INT NOT NULL ,
+  `person_id` INT NOT NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `person_id` (`person_id` ASC) ,
+  CONSTRAINT
+    FOREIGN KEY (`person_id` )
+    REFERENCES `kamilpomyk_baza`.`person` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `kamilpomyk_baza`.`student_guardian`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `kamilpomyk_baza`.`student_guardian` ;
+
+CREATE  TABLE IF NOT EXISTS `kamilpomyk_baza`.`student_guardian` (
+  `student_id` INT NOT NULL ,
+  `guardian_id` INT NOT NULL ,
+  INDEX `student_id` (`student_id` ASC) ,
+  INDEX `guardian_id` (`guardian_id` ASC) ,
+  CONSTRAINT
+    FOREIGN KEY (`student_id` )
+    REFERENCES `kamilpomyk_baza`.`student` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT
+    FOREIGN KEY (`guardian_id` )
+    REFERENCES `kamilpomyk_baza`.`guardian` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
